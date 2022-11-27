26281d012b99433882139f5bf32eeae4cc2e59a9
Kilian-Pichard <kilianpichard@aol.fr>
2022-11-27 11:16:21 +0100

    Modification texte fichier AppCertif.py

--- AppCertif.py
+++ AppCertif.py
@@ -4,17 +4,17 @@
 from CreerAttestation import *
 from ExtrairePreuve import *
 
-print("Bonjour, bienvenue sur l'application de certification de CY Tech ! Ici, vous pourrez créer un certificat, ainsi que verifier la validité de votre certificat.")
+print("Bonjour, bienvenue sur l'application d'attestation de CY Tech ! Ici, vous pourrez créer une attestation, ainsi que verifier la validité de votre attestation.")
 print("Veuillez choisir ce que vous souhaitez faire :")
 
 choix_default = 1
-choix = int(input("1- Créer un certificat\n2- Vérifier la validité de votre certificat\n0- Quitter le programme\nVotre choix [1] : ") or 1)
+choix = int(input("1- Créer une attestation\n2- Vérifier la validité de votre attestation\n0- Quitter le programme\nVotre choix [1] : ") or 1)
 print("")
 if choix == 1:
-	print("Vous avez choisi de créer un certificat.")
+	print("Vous avez choisi de créer une attestation.")
 	creer_attestation()
 elif choix == 2:
-	print("Vous avez choisi de vérifier la validité de votre certificat.")
+	print("Vous avez choisi de vérifier la validité de votre attestation.")
 	extraire_preuve()
 elif choix == 0:
 	print("Vous avez choisi de quitter le programme.")
